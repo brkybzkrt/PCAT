e5372f75f98724983d8f381a3cfb384c77110d3c
brkybzkrt <b9bozkurt959@gmail.com>
2021-10-11 11:17:37 +0300

    Mongoose added and Photo model created

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const Photo = require('./models/Photo');
 const ejs = require('ejs');
 
+const path = require('path');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/pcat', {
+  usenewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //template engine
 app.set('view engine', 'ejs');
 
+//middlewares
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //routes
-app.get('/', (req, res) => {
-  res.render('index');
-  
+app.get('/', async (req, res) => {
+  const photos = await Photo.find().sort({ created_date: -1 });
+
+  res.render('index', { photos });
 });
 
-app.get("/about",(req,res)=>{
-  res.render('about')
-})
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-app.get('/add',(req,res)=>{
-  res.render('add')
-})
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.get('/photo/:id', async (req, res) => {
+  const id = req.params.id;
+  const photo = await Photo.findById(id);
+
+  res.render('imagePage', { photo });
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 
