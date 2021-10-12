6cc383b382ab8ca3eafab4c1882aaf4cbecf0df4
brkybzkrt <b9bozkurt959@gmail.com>
2021-10-12 14:43:36 +0300

    Express-fileupload added and some pages edited

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Photo = require('./models/Photo');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
+const fs = require('fs');
+
+const Photo = require('./models/Photo');
 
 const path = require('path');
 
@@ -20,6 +23,8 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(fileUpload());
 
 //routes
 app.get('/', async (req, res) => {
@@ -44,8 +49,22 @@ app.get('/photo/:id', async (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: `/uploads/${uploadedImage.name}`,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
